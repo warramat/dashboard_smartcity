27917dd8ce162afa323e2035f2114558bef5b6c5
warramat1999 <warramat1999@gmail.com>
2022-10-25 10:02:48 +0700

    map

--- appeal/appeal/appealEdit.js
+++ appeal/appeal/appealEdit.js
@@ -1,18 +1,18 @@
-var jwt = localStorage.getItem('jwt');
+var jwt = localStorage.getItem("jwt");
 if (jwt == null) {
-  window.location.href = '../../login.html';
+  window.location.href = "../../login.html";
 }
 
 function readJWT() {
-  var base64Url = jwt.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var base64Url = jwt.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   var jsonPayload = decodeURIComponent(
     atob(base64)
-      .split('')
+      .split("")
       .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
       })
-      .join('')
+      .join("")
   );
   const obj = JSON.parse(jsonPayload);
   return obj;
@@ -20,24 +20,24 @@ function readJWT() {
 
 function loadUser() {
   const obj = readJWT();
-  document.getElementById('userid').innerText = obj.userid;
-  document.getElementById('adminname').innerText = obj.name;
+  document.getElementById("userid").innerText = obj.userid;
+  document.getElementById("adminname").innerText = obj.name;
 }
 
 loadUser();
 
 function logout() {
-  localStorage.removeItem('jwt');
-  window.location.href = '../../login.html';
+  localStorage.removeItem("jwt");
+  window.location.href = "../../login.html";
 }
 
 window.onload = async () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const _id = urlParams.get('_id');
+  const _id = urlParams.get("_id");
   let url =
-    'https://smartcity-pakpoon-api.herokuapp.com/appeal/userData/?id=' + _id;
+    "https://smartcity-pakpoon-api.herokuapp.com/appeal/userData/?id=" + _id;
   if ((await (await fetch(url)).status) !== 200) {
-    document.write('not found.');
+    document.write("not found.");
     return;
   }
   let data = await (await fetch(url)).json();
@@ -46,8 +46,8 @@ window.onload = async () => {
 };
 
 function setPage(data) {
-  const img_url = 'https://smartcity-pakpoon-api.herokuapp.com/userSmart/';
-  document.getElementById('pictureUrl').src = data.pictureUrl;
+  const img_url = "https://smartcity-pakpoon-api.herokuapp.com/userSmart/";
+  document.getElementById("pictureUrl").src = data.pictureUrl;
   let H = Object.keys(data);
   H.forEach((e) => {
     try {
@@ -57,39 +57,45 @@ function setPage(data) {
 
   try {
     document.getElementById(
-      'address'
+      "address"
     ).innerText = `${data.housenumber} ${data.sub_district} ${data.district} ${data.province} ${data.zipcode}`;
 
     document.getElementById(
-      'name'
+      "name"
     ).innerText = `${data.prefix} ${data.name} ${data.lastname}`;
-    document.getElementById('created1').innerText = data.created;
-    document.getElementById('adminstart').innerText = ` ${toThaidate(
+    //document.getElementById("created1").innerText = data.created;
+   /* document.getElementById("adminstart").innerText = ` ${toThaidate(
       data.created
-    )} เวลา ${getTime(data.created)} โดย ${data.name} ${data.lastname}`;
+    )} เวลา ${getTime(data.created)} โดย ${data.name} ${data.lastname}`;*/
 
-    document.getElementById('adminend').innerText = ` ${toThaidate(
+    /*document.getElementById("adminend").innerText = ` ${toThaidate(
       data.edit
-    )} เวลา ${getTime(data.edit)} โดย ${data.admin_startData.name}`;
+    )} เวลา ${getTime(data.edit)} โดย ${data.admin_startData.name}`;*/
 
-    document.getElementById('end').innerText = ` ${toThaidate(
+
+    /*document.getElementById("end").innerText = ` ${toThaidate(
       data.end
-    )} เวลา ${getTime(data.end)} โดย ${data.admin_startData.name}`;
+    )} เวลา ${getTime(data.end)} โดย ${data.admin_startData.name}`;*/
+    /*console.log(`${toThaidate(
+      data.end
+    )} เวลา ${getTime(data.end)} โดย ${data.admin_startData.name}`);*/
 
-    document.getElementById('response_time').innerText = ` ${toThaidate(
+   /* document.getElementById("response_time").innerText = ` ${toThaidate(
       data.response_time
-    )} เวลา ${getTime(data.response_time)}`;
+    )} เวลา ${getTime(data.response_time)}`;*/
 
     document.getElementById(
-      'map'
+      "map"
     ).href = `https://www.google.com/maps?q=${data.gps.lat},${data.gps.lng}`;
-    document.getElementById('tel').href = 'tel:' + data.telephone;
-    document.getElementById('tel1').innerText = data.telephone;
-    document.getElementById('status1').innerText = data.status;
-    document.getElementById('end1').innerText = data.end;
+    document.getElementById("tel").href = "tel:" + data.telephone;
+    document.getElementById("tel1").innerText = data.telephone;
+    document.getElementById("status1").innerText = data.status;
+    document.getElementById("end1").innerText = data.end;
     renderStar(data.Star);
-  } catch (e) {}
-  let html_img = '';
+  } catch (e) {
+    console.log(e);
+  }
+  let html_img = "";
   data.img.forEach((e) => {
     html_img += `
     <li class="item">
@@ -98,8 +104,8 @@ function setPage(data) {
       </a>
     </li>`;
   });
-  document.getElementById('preview').innerHTML = html_img;
-  html_img = '';
+  document.getElementById("preview").innerHTML = html_img;
+  html_img = "";
   data.add_img.forEach((e) => {
     html_img += `
     <li class="item">
@@ -108,8 +114,10 @@ function setPage(data) {
       </a>
     </li>`;
   });
-  document.getElementById('add_img').innerHTML = html_img;
-  if (data.response_time === '') {
+  if (document.getElementById("add_img")) {
+    document.getElementById("add_img").innerHTML = html_img;
+  }
+  if (data.response_time === "") {
     showmsg();
   }
 }
@@ -117,39 +125,39 @@ function setPage(data) {
 function commit() {
   const adminData = readJWT();
   const urlParams = new URLSearchParams(window.location.search);
-  const _id = urlParams.get('_id');
+  const _id = urlParams.get("_id");
   const url =
-    'https://smartcity-pakpoon-api.herokuapp.com/appeal/editappeal/' + _id;
+    "https://smartcity-pakpoon-api.herokuapp.com/appeal/editappeal/" + _id;
   Swal.fire({
-    icon: 'question',
-    title: 'ยืนยันการรับงาน ?',
+    icon: "question",
+    title: "ยืนยันการรับงาน ?",
     showDenyButton: true,
     showCancelButton: false,
-    confirmButtonText: 'ยืนยัน',
-    denyButtonText: 'ยกเลิก'
+    confirmButtonText: "ยืนยัน",
+    denyButtonText: "ยกเลิก",
   }).then((result) => {
     if (result.isConfirmed) {
       var myHeaders = new Headers();
-      myHeaders.append('Content-Type', 'application/json');
+      myHeaders.append("Content-Type", "application/json");
       let raw = JSON.stringify({
-        id: adminData.id
+        id: adminData.id,
       });
       var requestOptions = {
-        method: 'PUT',
+        method: "PUT",
         headers: myHeaders,
         body: raw,
-        redirect: 'follow'
+        redirect: "follow",
       };
 
       fetch(url, requestOptions)
         .then((response) => response.text())
         .then((result) =>
-          Swal.fire('บันทึกข้อมูลสำเร็จ!', '', 'success').then(() => {
-            window.location = './appeal.html';
+          Swal.fire("บันทึกข้อมูลสำเร็จ!", "", "success").then(() => {
+            window.location = "./appeal.html";
           })
         )
         .catch((error) =>
-          Swal.fire('เกิดข้อผิดพลาด', error, 'error').then(() => {
+          Swal.fire("เกิดข้อผิดพลาด", error, "error").then(() => {
             location.reload();
           })
         );
@@ -158,9 +166,9 @@ function commit() {
 }
 
 function renderStar(Star) {
-  let html = '';
+  let html = "";
   if (Star === 0) {
-    document.getElementById('star').innerHTML = '-';
+    document.getElementById("star").innerHTML = "-";
     return;
   }
   for (let i = 0; i < 5; i++) {
@@ -170,15 +178,17 @@ function renderStar(Star) {
       html += '<span class="fa fa-star"></span>';
     }
   }
-  document.getElementById('star').innerHTML = html;
+  document.getElementById("star").innerHTML = html;
 }
 
 function showmsg() {
-  document.getElementById('msg').innerHTML = ` 
+  if (document.getElementById("msg")) {
+    document.getElementById("msg").innerHTML = ` 
   <button type="button" onclick="send_message()" id="close"
   style="background-color:#229a58;color:white;margin: 0px 5px;"
   class="btn btn-primary align-self-center">ส่งข้อความ</button>
 <button type="button" onclick="nosend_message()" id="close"
   style="background-color:#b81d43;color:white;margin: 0px 5px;"
   class="btn btn-primary align-self-center">ไม่ส่งข้อความ</button>`;
+  }
 }
--- appeal/appeal/appealExamine.js
+++ appeal/appeal/appealExamine.js
@@ -1,29 +1,33 @@
-var jwt = localStorage.getItem('jwt');
+var jwt = localStorage.getItem("jwt");
 if (jwt == null) {
-  window.location.href = '../../login.html';
+  window.location.href = "../../login.html";
 }
 
 function loadUser() {
-  var base64Url = jwt.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var base64Url = jwt.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   var jsonPayload = decodeURIComponent(
     atob(base64)
-      .split('')
+      .split("")
       .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
       })
-      .join('')
+      .join("")
   );
   const obj = JSON.parse(jsonPayload);
-  document.getElementById('userid').innerText = obj.userid;
-  document.getElementById('name').innerText = obj.name;
+  if (document.getElementById("userid")) {
+    document.getElementById("userid").innerText = obj.userid;
+  }
+  if (document.getElementById("name")) {
+    document.getElementById("name").innerText = obj.name;
+  }
 }
 
 loadUser();
 
 function logout() {
-  localStorage.removeItem('jwt');
-  window.location.href = '../../login.html';
+  localStorage.removeItem("jwt");
+  window.location.href = "../../login.html";
 }
 
 /*************************************************** */
@@ -38,68 +42,68 @@ function loadFile(event) {
 
 function addImage(img) {
   const id = Math.round(Math.random() * 10000);
-  $('#list_images').append(`
+  $("#list_images").append(`
     <li class="item" id="${id}">
       <img class="img-row" src="${img}">
       <button class="btn btn-danger" onclick="remove_img(${id})">x</button>
     </li>
     `);
-  $('#upload').val('');
-  $('#camera').val('');
+  $("#upload").val("");
+  $("#camera").val("");
 }
 
 function remove_img(id) {
-  $('#' + id).remove();
+  $("#" + id).remove();
 }
 
 function submitAppeal() {
   Swal.fire({
-    icon: 'question',
-    title: 'ยืนยันการรับงาน ?',
+    icon: "question",
+    title: "ยืนยันการรับงาน ?",
     showDenyButton: true,
     showCancelButton: false,
-    confirmButtonText: 'ยืนยัน',
-    denyButtonText: 'ยกเลิก'
+    confirmButtonText: "ยืนยัน",
+    denyButtonText: "ยกเลิก",
   }).then(async (result) => {
     if (result.isConfirmed) {
       const adminData = readJWT();
       var myHeaders = new Headers();
-      myHeaders.append('Content-Type', 'application/json');
+      myHeaders.append("Content-Type", "application/json");
       const urlParams = new URLSearchParams(window.location.search);
-      const _id = urlParams.get('_id');
+      const _id = urlParams.get("_id");
       let img = [];
-      $('.img-row').each(function (i, obj) {
-        img.push($(this).attr('src'));
+      $(".img-row").each(function (i, obj) {
+        img.push($(this).attr("src"));
       });
       var requestOptions = {
-        method: 'POST',
+        method: "POST",
         headers: myHeaders,
         body: JSON.stringify({
           add_img: img,
-          detail_edit: $('#comment').val()
+          detail_edit: $("#comment").val(),
         }),
-        redirect: 'follow'
+        redirect: "follow",
       };
       try {
         await fetch(
-          'https://smartcity-pakpoon-api.herokuapp.com/appeal/submitappeal/' +
+          "https://smartcity-pakpoon-api.herokuapp.com/appeal/submitappeal/" +
             _id,
           requestOptions
         );
-        requestOptions.method = 'PUT';
+        requestOptions.method = "PUT";
         requestOptions.body = JSON.stringify({
-          id: adminData.id
+          id: adminData.id,
         });
         await fetch(
-          'https://smartcity-pakpoon-api.herokuapp.com/appeal/editappeal/' +
+          "https://smartcity-pakpoon-api.herokuapp.com/appeal/editappeal/" +
             _id,
           requestOptions
         );
-        Swal.fire('บันทึกข้อมูลสำเร็จ!', '', 'success').then(() => {
-          window.location = './appeal.html';
+        Swal.fire("บันทึกข้อมูลสำเร็จ!", "", "success").then(() => {
+          window.location = "./appeal.html";
         });
       } catch (e) {
-        Swal.fire('เกิดข้อผิดพลาด', String(e), 'error').then(() => {
+        Swal.fire("เกิดข้อผิดพลาด", String(e), "error").then(() => {
           location.reload();
         });
       }
